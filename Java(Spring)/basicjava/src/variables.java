8e9f71ee184d297fa3bceeab4000d0748c8063f7
Hooni07 <gkxogns21@gmail.com>
2025-03-05 13:50:46 +0900

    add 03/05 study files

---
+++ Java(Spring)/basicjava/src/variables.java
@@ -0,0 +1,8 @@
+public class variables {
+    public static void main(String[] args) {
+        int num = 1;
+        double width = 12.34;
+        String content = "Programming";
+        System.out.println(num);
+    }
+}
