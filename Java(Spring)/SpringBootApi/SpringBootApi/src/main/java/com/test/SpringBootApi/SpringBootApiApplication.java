442acfd149ddb867fc3a0e756a72247f92ba0d94
Hooni07 <gkxogns21@gmail.com>
2025-03-14 13:03:12 +0900

    Add 03/14 Study(Spring)

--- Java(Spring)/SpringBootApi/SpringBootApi/src/main/java/com/test/SpringBootApi/SpringBootApiApplication.java
+++ Java(Spring)/SpringBootApi/SpringBootApi/src/main/java/com/test/SpringBootApi/SpringBootApiApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.test.SpringBootApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SpringBootApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SpringBootApiApplication.class, args);
 	}
 
 }
--- Java(Spring)/SpringBootApi/SpringBootApi/src/test/java/com/test/SpringBootApi/SpringBootApiApplicationTests.java
+++ Java(Spring)/SpringBootApi/SpringBootApi/src/test/java/com/test/SpringBootApi/SpringBootApiApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.test.SpringBootApi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class SpringBootApiApplicationTests {
 
 	@Test
 	void contextLoads() {
